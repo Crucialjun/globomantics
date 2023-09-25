cabf0e97e8e9672519409a007507d372d75134ea
nich.otieno <nicholas.otieno@revca.tech>
2023-09-25 18:40:24 +0300

    add

---
+++ src/main_page/index.js
@@ -0,0 +1,49 @@
+import "./main_page.css";
+
+import Header from "./header";
+import { useEffect, useMemo, useState } from "react";
+import {
+  Switch,
+  BrowserRouter,
+  Route,
+} from "react-router-dom/cjs/react-router-dom";
+import FeaturedHouse from "./featured-house";
+import SearchResults from "../search-results";
+import HouseFilter from "./house-filter";
+
+function App() {
+  const [allHouses, setAllHouses] = useState([]);
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const rsp = await fetch("/houses.json");
+      const houses = await rsp.json();
+      setAllHouses(houses);
+    };
+    fetchHouses();
+  }, []);
+
+  const featuredHouse = useMemo(() => {
+    if (allHouses.length) {
+      const randomIndex = Math.floor(Math.random() * allHouses.length);
+      return allHouses[randomIndex];
+    }
+  }, [allHouses]);
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <Header subtitle="Providing houses all over the world props"></Header>
+        <HouseFilter allHouses={allHouses}></HouseFilter>
+        <Switch>
+          <Route path="/searchresults/:country">
+            <SearchResults allHouses={allHouses}></SearchResults>
+          </Route>
+          <Route exact path="/">
+            <FeaturedHouse house={featuredHouse}></FeaturedHouse>
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
